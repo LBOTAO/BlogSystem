3b30eb5d2ecc0c123dad47e57acfad83847a7c99
liutao <1538286083@qq.com>
2020-04-17 09:10:18 +0800

    swagger的配置

--- src/main/java/com/blog/controller/ArticleController.java
+++ src/main/java/com/blog/controller/ArticleController.java
@@ -2,6 +2,8 @@ package com.blog.controller;
 
 import com.blog.entity.Article;
 import com.blog.service.ArticleService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +12,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/article")
+@Api(value="文章管理",description="测试接口描述")
 public class ArticleController {
 
     @Autowired
@@ -17,6 +20,7 @@ public class ArticleController {
 
     @RequestMapping("/selectByPrimaryKey/{id}")
     @ResponseBody
+    @ApiOperation(value="按文章ID查询文章详细信息",httpMethod="GET",notes="在没有会话、没有签名的情况下，进入方法体")
     public Object selectByPrimaryKey(@PathVariable Integer id){
         Article article = articleService.selectByPrimaryKey(id);
         return article;
